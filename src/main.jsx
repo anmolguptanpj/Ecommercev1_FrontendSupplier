e5fa735931395bd372947a1893c55958a370867f
anmolguptanpj <anmolguptanpj282@gmail.com>
2025-11-24 18:30:04 +0530

    inrtrodcued tan query , made category creation page

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,16 @@ import './index.css'
 import App from './App.jsx'
 import { Provider } from 'react-redux'
 import { store } from './store/store.js'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient =  new QueryClient()
 
 createRoot(document.getElementById('root')).render(
+<QueryClientProvider client={queryClient}>
 <Provider store={store}>
      <App />
 </Provider>
+</QueryClientProvider>
  
   ,
 )
