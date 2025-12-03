860cc4c9ded14892f87cefa8b7250699242ceea9
anmolguptanpj <anmolguptanpj282@gmail.com>
2025-12-03 21:00:44 +0530

    Added input boxes in  Edit page

--- src/pages/Products/EditProducts.jsx
+++ src/pages/Products/EditProducts.jsx
@@ -1,9 +1,12 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams,useNavigate } from 'react-router-dom'
 import api from '../../api'
 import { useQuery } from '@tanstack/react-query'
 
 function EditProducts() {
+ 
+/************************************** View Space **********************/
+    
     const[edit,setEdit] = useState(false);
     const navigate = useNavigate()
     const{id} = useParams()
@@ -26,10 +29,80 @@ const fetchProduct = async() => {
  const product = data?.data
  console.log(product)
 
+
+/******************** Edit Form *******************/  
+
+const [form,SetForm] = useState({
+name:"",
+brand:"",
+subCategory:"",
+category:"",
+mrp:"",
+discount:"",
+isActive:"",
+productImage:[],
+description:{
+    minimalInformation:"",
+    brief:"",
+    whatsInThebox:[],
+    aboutThisItem:[],
+    productSpecification:{
+
+    },
+}
+})
+
+
+useEffect(()=>{
+if(product){
+    SetForm({
+name:product.name || "",
+brand:product.brand?.name || "",
+subCategory:product.subCategory?.name||"",
+category:product.category?.name||"",
+mrp:product.mrp||"",
+discount:product.discount||"",
+isActive:product.isActive||"",
+productImage:product.productImage,
+description:{
+    minimalInformation:product.description.minimalInformation||"",
+    brief:product.description.brief||"",
+    aboutThisItem:product.description?.aboutThisItem||"",
+    whatsInThebox:product.description?.whatsInThebox||"",
+    productSpecification:product.description?.productSpecification||""
+
+    
+}
+        
+
+    })
+}
+},[product])
+
+
+const handleChange = (e)=>{
+    SetForm({...form,[e.target.name]:e.target.value})
+}
+
+const handleEdit =() =>{
+    
+}
+
+console.log(form)
+
+
+
+
+
+
+
+
   return (
     <div className='flex   w-full h-full '>
 
         { edit=== false  ? 
+
+        /******************* View function UI ************ */
            ( <div className='w-full h-15 px-4'>
             <div className='w-full h-15 rounded-2xl bg-rose-400 items-center flex justify-center items text-3xl font-bold py-6'>Product Details</div>
          <div className='flex flex-col gap-2 py-3  w-full justify-center'>
@@ -127,13 +200,85 @@ const fetchProduct = async() => {
             
         </div>) : (
         
-            <div>
-               <div> <button
-                className='px-4 py-1 rounded-xl bg-green-500 '
-                onClick={()=>{
-                setEdit(false);
-                console.log(edit)
-                }}> Back</button>  </div>
+
+        /******************* Edit function UI *************/
+            <div className='px-4 w-full h-full '>
+                    <div className=' flex flex-col border-2 justify-center '>
+                        <form className='flex flex-col gap-3 px-6 py-6' onSubmit={handleEdit()}>
+                        <fieldset>
+                            <legend>Product Introduction and Pricing</legend>
+                             <div>
+                                    <label>Product Name</label>
+                                    <div>
+                                        <input name='name' placeholder='Write your product Name' className='border-2' value={form.name} onChange={handleChange}/>
+                                    </div>
+                            </div>
+                            <div>
+                                    <label>Brand</label>
+                                    <div>
+                                        <select name='brand' className='border-2' value={form.brand} onChange={handleChange}>
+                                        <option value={product.brand.id}>{form.brand}</option>
+                                        </select>
+                                    </div>
+                            </div>
+                            <div>
+                                    <label>Category</label>
+                                    <div>
+                                        <select name='category' className='border-2' value={form.category} onChange={handleChange}>
+                                       <option value={product.category.id}>{form.category}</option>
+                                        </select>
+                                    </div>
+                            </div>
+                             <div>
+                                    <label>SubCategory</label>
+                                    <div>
+                                        <select name='subCategory' className='border-2' value={form.subCategory} onChange={handleChange}>
+                                       <option value={product.subCategory.id}>{form.subCategory}</option>
+                                        </select>
+                                    </div>
+                            </div>
+                            <div>
+                                    <label>MRP</label>
+                                    <div>
+                                        <input type='number' name='mrp' placeholder='Write your Product MRP' className='border-2' value={form.mrp} onChange={handleChange}/>
+                                    </div>
+                            </div>
+                            <div>
+                                    <label>Discount %</label>
+                                    <div>
+                                        <input name='discount' placeholder='Write the appropraite discount %' className='border-2' value={form.discount} onChange={handleChange}/>
+                                    </div>
+                            </div>
+                            <div>
+                                    <label>SalesPrice</label>
+                                    <div>
+                                        <input  placeholder='Write your Product MRP' disabled className='border-2' value={form.mrp*((100-(form.discount))/100)} readOnly />
+                                    </div>
+                            </div>
+                            <div>
+                                    <label>Discount %</label>
+                                    <div>
+                                        <input name='mrp' placeholder='Write your Product MRP' className='border-2' value={form.discount} onChange={handleChange}/>
+                                    </div>
+                            </div>
+                    
+                        </fieldset>
+                        </form>
+
+
+
+                    </div>
+
+
+
+
+
+
+                <div> <button
+                    className='px-4 py-1 rounded-xl bg-green-500 '
+                    onClick={()=>{
+                    setEdit(false);
+                    }}> Back</button>  </div>
             </div>
         
         )
