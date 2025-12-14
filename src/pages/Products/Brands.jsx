f9bdaf4fd46371fb2cc55e21948e88188dcff6a7
anmolguptanpj <anmolguptanpj282@gmail.com>
2025-12-15 00:01:25 +0530

    UI

--- src/pages/Products/Brands.jsx
+++ src/pages/Products/Brands.jsx
@@ -31,14 +31,20 @@ function Brands() {
   }
   return (
     <div>
-      <div>
-        <div> </div>
-        <div>
-          <form onSubmit={handleBrand}>
-            <input type='text' value={brand} onChange={(e)=>setBrand(e.target.value)}/>
-            <button type='submit'>{isCreatingBrand ? 'Creating...' : 'Create Brand'} </button>
-            {createBrandIsError && (<p>Error : {createBrandError?.message || 'Error creating category'}</p>)}
+      <div className='w-full flex justify-center'>
+        <div className=''>
+          <form className='p-4' onSubmit={handleBrand}>
+          <fieldset className='p-5 border-3'>
+            <legend>Brand</legend>
+             <label>Brand:</label>
+       
+             
+             <div className='border-2'> <input className='w-100 focus:outline-none' type='text' value={brand} onChange={(e)=>setBrand(e.target.value)}/></div>
+           <div className='w-100 flex justify-center p-3'> <button className=' px-2 rounded-xl bg-green-500 hover:bg-blue-500' type='submit'>{isCreatingBrand ? 'Creating...' : 'Create Brand'} </button></div>
+           <div className='h-5 w-100'> {createBrandIsError && (<p>Error : {createBrandError?.message || 'Error creating category'}</p>)}</div>
+          
 
+          </fieldset>
             
           </form>
         </div>
