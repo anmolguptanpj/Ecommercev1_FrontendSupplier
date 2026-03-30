5c36968ffd2f07a4115fea74fe0327f65edf3b87
anmolguptanpj <anmolguptanpj282@gmail.com>
2026-03-30 17:04:17 +0530

    made the order invoice look more specific

--- src/modals/OrderView.jsx
+++ src/modals/OrderView.jsx
@@ -1,16 +1,41 @@
 import React from 'react'
 import { useExtraDetails } from '../hooks/useExtraDetails'
+import { useQuery } from '@tanstack/react-query'
+import api from '../api'
 
 
 
 
-function OrderView() {
+function OrderView({_id,onClose}) {
   const extra = useExtraDetails()
-  console.log(extra)
+  const address = extra.address
+  const supplierId = extra._id
+  console.log(_id)
+  console.log(supplierId)
+
+  const getOrder = async () => {
+    const res = await api.get(`/${supplierId}/${_id}`)
+    return res.data
+  }
+
+  const {data,isLoading,isError} = useQuery({
+    queryKey:["order",_id,supplierId],
+    queryFn:getOrder,
+    enabled: !!_id && !!supplierId
+  })
+
+  if(isLoading) return <p>Loading Order...</p>
+  if(isError) return <p> Fetching Order failed</p>
+  const order = data?.data || []
+  const items = order.items
+
+
+
   return (
     <div className='w-full h-full fixed inset-0 bg-white text-black'>
-            <div className='w-full flex justify-center text-4xl'>
-              <div>{extra.supplierName}</div>
+            <div className='w-full flex flex-col items-center justify-center text-4xl'>
+              <div className='text-4xl'>{extra.supplierName}</div>
+              <div className='text-xl'>{`${address.houseNo},${address.street},${address.city}-${address.pincode},${address.state}`}</div>
             </div>
 
 
@@ -18,14 +43,14 @@ function OrderView() {
                                 <div className='
                               flex flex-col'>
                               <p>Document Details</p>
-                                <div className='flex justify-between'>
-                                  <div className='grid grid-cols-1 grid-rows-2'>
-                                <p>Order Origin No: {"Not Available"}</p>
-                                <p>Order No : {"Not Available"}</p>
+                                <div className='flex justify-between px-2'>
+                                  <div className='grid grid-cols-2 grid-rows-2'>
+                                <p>Order Origin No:</p><p>{order.orderNo||"Not Available"}</p>
+                                <p>Order No :</p><p>{order.supplierOrderNo||"Not Available"}</p>
                                 </div>
-                                  <div className='grid grid-cols-1 grid-rows-2'>
-                                <p>Order Origin Date: {"Not Available"}</p>
-                                <p>Todays Date: {"Not Available"}</p>
+                                  <div className='grid grid-cols-2 grid-rows-2'>
+                                <p>Order Origin Date:</p><p> { new Date(order.createdAt).toLocaleString() || "Not Available"}</p>
+                                <p>Todays Date:</p><p> {(new Date).toLocaleString()}</p>
                                 </div>
                                 </div>
                               </div>
@@ -33,10 +58,10 @@ function OrderView() {
                             <div className='
                             flex flex-col'>
                             <p>  Customer Details</p>
-                            <div className='flex  justify-between'>
-                              <div className='grid grid-cols-1 grid-rows-2'>
-                              <p>Name:{"Not Available"}</p>
-                                <p>Address: {"Not Available"}</p>
+                            <div className='flex  justify-between px-2 '>
+                              <div className='grid grid-cols-2 grid-rows-2'>
+                              <p>Name:</p><p>{order.userName||"Not Available"}</p>
+                              <p>Address:</p><p>{order.deliveryAddress || "Not Available"}</p>
                               </div>
                               </div>
                               </div>
@@ -47,18 +72,54 @@ function OrderView() {
 
                               <table>
                                 <thead>
-                                  <tr>
+                                  <tr className='w-full grid grid-cols-6 ' >
                                     <th>Item No</th>
                                     <th>Item Name</th>
-                                    <th>Iten Qty</th>
+                                    <th>Item Qty</th>
                                     <th>Item Price</th>
+                                    <th>Total</th>
                                     <th>Status</th>
                                   </tr>
                                 </thead>
                                 <tbody>
-                                  
+                                 {items.map((i,index)=>(<>
+                                 <tr className='w-full grid grid-cols-6 ' key={items._id}>
+                                  {index+1}
+                                 <td>
+                                  {i.name}
+                                 </td>
+                                 <td>
+                                  {i.quantity}
+                                 </td>
+                                 <td>
+                                  {i.unitPrice}
+                                 </td>
+                                 <td>
+                                  {i.quantity * i.unitPrice}
+                                 </td>
+                                 <td>
+                                  {i.status || "pending"}
+                                 </td>
+                                 </tr>
+                                 </>))}
+                                 <tr className='grid grid-cols-3 grid-rows-1' >
+                                  <th className='grid grid-cols-2'>
+                                    Total
+                                  </th>
+                                  <td className='grid grid-cols-1'>
+                                   {order.totalQty}
+                                  </td>
+                                  <td className='grid grid-cols-1'>
+                                  {order.totalPrice}
+                                  </td>
+                                 </tr>
                                 </tbody>
                               </table>
+                  </div>
+
+                  <div className='flex justify-end p-6 '>
+                    <button onClick={onClose}
+                    className='bg-green-500 px-2 py-1 rounded-xl'>Back</button>
                   </div>
     </div>
   )
